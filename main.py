c268af1018fd84b0e7966c4c9f213167dfa7072e
suman <sumanrbt1997@gmail.com>
2024-07-10 19:44:16 +0530

    (feat): initial project setup

---
+++ main.py
@@ -0,0 +1,12 @@
+class TensorStorage:
+    def __init__(self):
+        pass
+
+    def add(self, tensor):
+        pass
+
+    def get_image(self, index):
+        pass
+
+if __name__ == "__main__":
+    pass
