7c87e144c5c58ecc0fc1bbc9ca45cd90e51f8a1e
suman <sumanrbt1997@gmail.com>
2024-07-23 17:49:48 +0530

    feat(sparse representations): added the following utilities 1. created a per tensor sparse representation

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import numpy as np
-from utils.utils import get_storage
+from matplotlib import pyplot as plt
+
+from utils.utils import get_storage, write_to_file
 from utils.dataset_utils import (OriginalDataset,
                                  delta_between_images,
                                  plot_image_array,
                                  plot_hist_array)
+
+from utils.sparse_representation import SparseRepresentation as SP
 
 
 class TensorStorage(dict):
@@ -22,27 +26,40 @@ class TensorStorage(dict):
     def add(self):
         """this function adds the data to the dictionary"""
         idx = len(self)
-        if idx % self.checkpoint == 0:
+        # special case where the checkpoint is zero
+        if self.checkpoint == 0:
+            self[idx] = self.original_dataset[idx]
+        elif idx % self.checkpoint == 0:
             self[idx] = self.original_dataset[idx]
         else:
-            """@todo here we need to perform some encoding and store the encoded tensor"""
-            ref_img = self[idx // self.checkpoint]
+            """Encoding"""
+            ref_img_idx = (idx // self.checkpoint) * self.checkpoint
+            ref_img = self[ref_img_idx]
             orig_img = self.original_dataset[idx]
             delta = delta_between_images(ref_img, orig_img)
+            # store the sparse representation
+            self.sp = SP(delta.shape)
+            sparse_matrix = self.sp.get_sparse_representation(delta)
             # print(f"{delta = }")
             # print(f"fraction of zero deltas: {np.count_nonzero(delta==0)/len(np.ndarray.flatten(delta)) = }")
-            self[idx] = delta
+            self[idx] = sparse_matrix
 
     def get_image(self, idx):
         """Here we need to reconstruct the original image and also verify that it is correct by
         comparing to the original image in the original dataset"""
         assert idx < len(self), (f"Trying to access idx {idx} which is not available in the TensorStorage,"
                                  f"available indices are {self.keys()}")
-        if idx % self.checkpoint == 0:
+        # special case where the checkpoint is 0
+        if self.checkpoint == 0:
+            return self[0]
+        elif idx % self.checkpoint == 0:
             return self[idx]
         else:
             # print(f"{self[idx // self.checkpoint][0,0,-1]} + {self[idx][0,0,-1]}")
-            return self[idx // self.checkpoint] + self[idx]
+            ref_img_idx = (idx // self.checkpoint) * self.checkpoint
+            out = self[ref_img_idx] + self.sp.get_dense_representation(self[idx])
+
+            return np.array(out, dtype=np.int64)
 
     def get_size(self):
         total_size = 0
@@ -56,22 +73,54 @@ class TensorStorage(dict):
     def plot_hist(self, idx):
         plot_hist_array(self[idx])
 
+    def plot_encoded_data_sizes(self):
+        sparse_matrices_size = [len(self[i])/(180*320) for i in range(1, len(self))]
+        print(f"{sparse_matrices_size = }")
+        plt.title(f"Sparse matrices sizes with checkpoint = {self.checkpoint}")
+        plt.xlabel(f"img idx")
+        plt.ylabel(f"len(sparse_matrix) %")
+        plt.plot(sparse_matrices_size)
+        plt.show()
+
 
 if __name__ == "__main__":
     original_dataset_ = OriginalDataset(data_path="./datasets/droid_100_sample_pictures")
-    img_0 = original_dataset_[0]
     tensor_storage = TensorStorage(checkpoint=10,
                                    original_dataset=original_dataset_)
     for idx in range(len(original_dataset_)):
+        print(f"adding image#{idx + 1} to the tensor storage")
         tensor_storage.add()
-    print(tensor_storage)
+    # print(tensor_storage)
     img_0 = tensor_storage.get_image(0)
     print(f'{img_0.shape = }')
     img_1 = tensor_storage.get_image(1)
     img_1_original = original_dataset_[1]
-    print(f"{img_1[0,0,-1] = }")
-    print(f"{img_1_original[0,0,-1] = }")
-    assert np.array_equal(img_1, img_1_original), "The original image and reconstructed img dont match!"
+    img_1_tensor_storage = tensor_storage.get_image(1)
+    ref_img1 = tensor_storage.get_image(0)
+    delta = delta_between_images(ref_img1, img_1_original)
+    print(f"{img_1_original[0,0] = }")
+    print(f"{img_1_tensor_storage[0,0] = }")
+    print(f"{ref_img1[0,0] = }")
+    print(f"{delta[0,0] = }")
+
+    orig_img18 = original_dataset_[18]
+    tensor_img18 = tensor_storage.get_image(18)
+    ref_img_for_img18 = tensor_storage.get_image(10)
+    delta = delta_between_images(ref_img_for_img18, orig_img18)
+    print(f"{ref_img_for_img18[7,194] = }")
+    print(f"{orig_img18[7,194] = }")
+    print(f"{tensor_img18[7,194] = }")
+    print(f"{delta[7,194] = }")
+    sp_mat = tensor_storage[1]
+    # print(f'{sp_mat[2316] = }, {len(sp_mat)}')
+    write_to_file(img_1_original, "img_1_original.txt")
+    write_to_file(img_1_tensor_storage, "img_1_tensor_storage.txt")
+
+    for i in range(len(original_dataset_)):
+        assert np.array_equal(original_dataset_[i], tensor_storage.get_image(idx=i)),\
+            f"The original image({i}) and reconstructed img({i}) dont match!"
+
+
     print(f"total size in MB of tensor storage: {tensor_storage.get_size()}")
     print(f"total size in MB of original dataset: {original_dataset_.get_storage_size()}")
 
@@ -79,3 +128,5 @@ if __name__ == "__main__":
 
     tensor_storage.plot_img(0)
     tensor_storage.plot_hist(1)
+
+    tensor_storage.plot_encoded_data_sizes()
