a00f8a357568ecffc7f9037add6bdf7c2c892bb5
Akash Bhansali <35609643+ASBJ021@users.noreply.github.com>
2024-07-28 21:09:35 +0200

    feat(campare algo): minto fixes

--- compare_compression_algo.py
+++ compare_compression_algo.py
@@ -4,16 +4,21 @@ import numpy as np
 from matplotlib import pyplot as plt
 import time
 from utils.dataset_utils import OriginalDataset
+from prettytable import PrettyTable
 
 def generate_comparison_table(performance_data):
-    table_header = "| Method | Compression Size (KB) | Original Size (KB) | Avg Compression Speed (s) | Avg Decompression Speed (s) | Compression Ratio |"
-    table_separator = "|--------|--------------------------|-----------------------|-----------------------|-------------------------|--------------------|"
-    table_rows = [
-        f"| {comp_algo} | {comp_size:.3f} | {orig_size:.3f} | {comp_time:.3f} | {decomp_time:.3f} | {comp_ratio:.3f} |"
-        for comp_algo, comp_size, orig_size, comp_time, decomp_time, comp_ratio in performance_data
-    ]
+    table = PrettyTable()
+    table.field_names = ["Compression Algo", "Compression Size (KB)", "Original Size (KB)", "Compression Time (s)", "Decompression Time (s)", "Compression Ratio"]
 
-    return "\n".join([table_header, table_separator] + table_rows)
+    for method, comp_size, orig_size, comp_time, decomp_time, comp_ratio in performance_data:
+        comp_size = f"{comp_size:.3f}"
+        orig_size = f"{orig_size:.3f}"
+        comp_time = f"{comp_time:.3f}"
+        decomp_time = f"{decomp_time:.3f}"
+        comp_ratio = f"{comp_ratio}"
+        table.add_row([method, comp_size, orig_size, comp_time, decomp_time, comp_ratio])
+
+    return table
 
 
 if __name__ == "__main__":
@@ -21,14 +26,12 @@ if __name__ == "__main__":
 
     comp_algo_list = ['FOR', 'delta']
     performance_data = []
-    indx = 5 #index number of image we want to reconstruct
 
     for comp_algo in comp_algo_list:
         tensor_storage = TensorStorage(checkpoint=10,
                                     original_dataset=original_dataset_,
                                     encoding_scheme= comp_algo,
                                     enlarge_factor=10)
-    # num_images = len(original_dataset_)
         num_images = 100
         print(f"#### Compressing  using {comp_algo} and storing {num_images} images #### ")
         comp_start_time = time.time()
@@ -53,7 +56,7 @@ if __name__ == "__main__":
         print(f"total size in KB of tensor storage: {comp_size}")
         print(f"total size in KB of original dataset: {orig_size}")
 
-        comp_ratio = orig_size/comp_size
+        comp_ratio = comp_size/orig_size
 
         performance_data.append([comp_algo,comp_size,orig_size ,total_comp_time, total_decomp_time, comp_ratio])
 
